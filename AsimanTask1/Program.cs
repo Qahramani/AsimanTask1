7b2f82a220eabe9f470adb3ea9ce4fa83ef4471b
testtttttttttt1234 <wapigix358@bsomek.com>
2024-06-28 23:15:57 +0400

    EnterLibrary Method extracted

--- AsimanTask1/Program.cs
+++ AsimanTask1/Program.cs
@@ -82,117 +82,7 @@ public class Program
                 }
                 break;
             case "4":
-            restartAddingBookToLibrary:
-                Console.WriteLine("Kitabxana sec:");
-                foreach (var item in libraries)
-                {
-                    Console.WriteLine(item);
-                }
-
-                Console.Write("Id daxil edin: ");
-                int libraryId = int.Parse(Console.ReadLine());
-
-                bool DoesLibraryExist = false;
-                foreach (var item in libraries)
-                {
-                    if (libraryId == item.Id)
-                    {
-                        DoesLibraryExist = true;
-                    restartLibraryMenu:
-                        Console.WriteLine("=======================\n Kitabxanan Menusu\n======================");
-                        Console.WriteLine("" +
-                            "[1]Kitab elave et\n" +
-                            "[2]Kitablari goster\n" +
-                            "[0]Kitabxanadan cix\n>>>");
-                        string opt2 = Console.ReadLine();
-                        switch (opt2)
-                        {
-                            case "1":
-                                Console.WriteLine("======== Movcud Kitablar =========");
-                                foreach (var book in books)
-                                {
-                                    Console.WriteLine(book);
-                                }
-
-                                Console.Write("Kitabin Idsin elave ediniz: ");
-                                int bookId = int.Parse(Console.ReadLine());
-
-                                bool doesBookExist = false;
-                                foreach (var book in books)
-                                {
-                                    if (bookId == book.Id)
-                                    {
-                                        doesBookExist = true;
-                                        break;
-                                    }
-                                }
-                                if (!doesBookExist)
-                                {
-                                    Colored.WriteLine("Bu IDli kitab tapilmadi", "red");
-                                    goto restartLibraryMenu;
-                                }
-                                bool DoesLibraryContainTheBook = false;
-
-                                if (item._books != null)
-                                {
-                                    foreach (var book in item._books)
-                                    {
-                                        if (book.Id == bookId)
-                                        {
-                                            Colored.WriteLine("Bu kitab artiq kitabxanada movcuddur", "red");
-                                            DoesLibraryContainTheBook = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (!DoesLibraryContainTheBook)
-                                    {
-                                        foreach (var book in books)
-                                        {
-                                            if (book.Id == bookId)
-                                            {
-                                                item.AddBook(book);
-                                                break;
-                                            }
-                                        }
-                                        Colored.WriteLine("Kitab elave olundu", "green");
-                                    }
-                                }
-                                else
-                                {
-                                    foreach (var book in books)
-                                    {
-                                        if (book.Id == bookId)
-                                        {
-                                            item.AddBook(book);
-                                            break;
-                                        }
-                                    }
-                                    Colored.WriteLine("Kitab elave olundu", "green");
-                                }
-
-
-                                break;
-                            case "2":
-                                Console.WriteLine($"====== {item.Name} kitabxanasinda olan butun kitablar ======");
-                                item.ListAllBooks();
-                                break;
-                            case "0":
-                                Colored.WriteLine("Kitabxanadan cixis etdiniz", "yellow");
-                                goto restartMainMenu;
-                            default:
-                                Colored.WriteLine("Duzgun deyer daxil ediniz", "red");
-                                break;
-                        }
-                        goto restartLibraryMenu;
-
-                    }
-                }
-                if (!DoesLibraryExist)
-                {
-                    Colored.WriteLine("Kitabxana ucun duzgun Id daxil ediniz", "red");
-                    goto restartAddingBookToLibrary;
-                }
+                EnterLibrary(libraries, books);
                 break;
             case "0":
                 Colored.WriteLine("Programdan cixis etdiniz", "yellow");
@@ -202,5 +92,120 @@ public class Program
                 break;
         }
         goto restartMainMenu;
+    }
+
+    private static void EnterLibrary(Library[] libraries, Book[] books)
+    {
+    restartAddingBookToLibrary:
+        Console.WriteLine("Kitabxana sec:");
+        foreach (var item in libraries)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.Write("Id daxil edin: ");
+        int libraryId = int.Parse(Console.ReadLine());
+
+        bool DoesLibraryExist = false;
+        foreach (var item in libraries)
+        {
+            if (libraryId == item.Id)
+            {
+                DoesLibraryExist = true;
+            restartLibraryMenu:
+                Console.WriteLine("=======================\n Kitabxanan Menusu\n======================");
+                Console.WriteLine("" +
+                    "[1]Kitab elave et\n" +
+                    "[2]Kitablari goster\n" +
+                    "[0]Kitabxanadan cix\n>>>");
+                string opt2 = Console.ReadLine();
+                switch (opt2)
+                {
+                    case "1":
+                        Console.WriteLine("======== Movcud Kitablar =========");
+                        foreach (var book in books)
+                        {
+                            Console.WriteLine(book);
+                        }
+
+                        Console.Write("Kitabin Idsin elave ediniz: ");
+                        int bookId = int.Parse(Console.ReadLine());
+
+                        bool doesBookExist = false;
+                        foreach (var book in books)
+                        {
+                            if (bookId == book.Id)
+                            {
+                                doesBookExist = true;
+                                break;
+                            }
+                        }
+                        if (!doesBookExist)
+                        {
+                            Colored.WriteLine("Bu IDli kitab tapilmadi", "red");
+                            goto restartLibraryMenu;
+                        }
+                        bool DoesLibraryContainTheBook = false;
+
+                        if (item._books != null)
+                        {
+                            foreach (var book in item._books)
+                            {
+                                if (book.Id == bookId)
+                                {
+                                    Colored.WriteLine("Bu kitab artiq kitabxanada movcuddur", "red");
+                                    DoesLibraryContainTheBook = true;
+                                    break;
+                                }
+                            }
+
+                            if (!DoesLibraryContainTheBook)
+                            {
+                                foreach (var book in books)
+                                {
+                                    if (book.Id == bookId)
+                                    {
+                                        item.AddBook(book);
+                                        break;
+                                    }
+                                }
+                                Colored.WriteLine("Kitab elave olundu", "green");
+                            }
+                        }
+                        else
+                        {
+                            foreach (var book in books)
+                            {
+                                if (book.Id == bookId)
+                                {
+                                    item.AddBook(book);
+                                    break;
+                                }
+                            }
+                            Colored.WriteLine("Kitab elave olundu", "green");
+                        }
+
+
+                        break;
+                    case "2":
+                        Console.WriteLine($"====== {item.Name} kitabxanasinda olan butun kitablar ======");
+                        item.ListAllBooks();
+                        break;
+                    case "0":
+                        Colored.WriteLine("Kitabxanadan cixis etdiniz", "yellow");
+                        return;
+                    default:
+                        Colored.WriteLine("Duzgun deyer daxil ediniz", "red");
+                        break;
+                }
+                goto restartLibraryMenu;
+
+            }
+        }
+        if (!DoesLibraryExist)
+        {
+            Colored.WriteLine("Kitabxana ucun duzgun Id daxil ediniz", "red");
+            goto restartAddingBookToLibrary;
+        }
     }
 }
